09300eb81f40df0bf9665c70ddce48bade80f10d
heyvipul <vipulgirhestar@gmail.com>
2023-12-30 16:35:22 +0530

    functionality

--- myapp/src/Pages/Home.jsx
+++ myapp/src/Pages/Home.jsx
@@ -1,10 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import "../App.css"
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 
 const Home = () => {
 
+  const[data,setData] = useState([])
+  const[loading,setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [sortOption, setSortOption] = useState(''); 
+  const [cart, setCart] = useState([]);
+
+  async function getProducts(){
+    try {
+      const response = await axios.get("https://dummyjson.com/products")
+      const data = response.data.products
+      console.log(data);
+      setData(data)
+      setLoading(false)
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getProducts()
+  },[])
+
+  useEffect(() => {
+    // Filter products based on search term
+    const filteredResults = data.filter((product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const sortedResults = filteredResults.sort((a, b) => {
+      if (sortOption === 'ascending') {
+        return parseFloat(a.price) - parseFloat(b.price);
+      } else if (sortOption === 'descending') {
+        return parseFloat(b.price) - parseFloat(a.price);
+      } else {
+        return 0;
+      }
+    });
+
+    setFilteredData(sortedResults);
+
+  }, [data,searchTerm,sortOption]);
+
+
+  if(loading){
+    return <h2 className='m-2'>Loading...</h2>
+  }
+
   return (
-    <div>Home</div>
+    <div>
+    <div id='functionality'>
+      <div>
+      <label htmlFor="">Search :</label>
+      <input type='text' placeholder='Search by product name'
+             onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+        <div>
+          <label>Sort By Price:</label>
+          <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+            <option value=''>Select</option>
+            <option value='ascending'>Ascending</option>
+            <option value='descending'>Descending</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Cart Count: {""}</p>
+          <p>
+            Total Amount:{' '} $
+          </p>
+      </div>
+      
+    </div>
+
+    <div id='products'>
+          {
+            filteredData?.map(function(ele,index){
+              return <div id='template'  key={index}>
+                <img src={ele.thumbnail} alt="" width="90%" height="300px" />
+                {/* <p>desc : {ele.description}</p> */}
+                <h4>Product : {ele.title}</h4>
+                <br />
+                  <div id='discount'>
+                    <h5>Price : {ele.price}$</h5>
+                    <h5>Dicount : {ele.discountPercentage}% Off</h5>
+                  </div>
+                  <MDBBtn className="m-4">Add to Cart!</MDBBtn>
+              </div>
+            })
+          }
+        </div>
+    </div>
   )
 }
 
